10cafbf759cf03e979aa41a53012c7045daddfa7
Carlos Solana Martínez <carlos.solana.martinez@gmail.com>
2020-02-07 23:53:24 +0100

    Add AnyCancellable Set

--- Examples/SkeletonUI-macOS-iOS/SkeletonUI-macOS-iOS/ViewModels/CharactersViewModel.swift
+++ Examples/SkeletonUI-macOS-iOS/SkeletonUI-macOS-iOS/ViewModels/CharactersViewModel.swift
@@ -3,13 +3,15 @@ import Combine
 class CharactersViewModel: ObservableObject {
     @Published var characters = [Character]()
     private let store = CharacterStore(service: CharacterService())
+    private var cancellables = Set<AnyCancellable>()
 
     func fetchCharacters() {
-        _ = store
+        store
             .fetchCharacters()
             .sink(receiveValue: { characters in
                 self.characters = characters
             })
+            .store(in: &cancellables)
     }
 
     func reloadCharacters() {
