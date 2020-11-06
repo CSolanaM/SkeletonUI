72a5d49a21eda8c7b1cfd1c151c2db9015faedd5
Carlos Solana Martínez <carlos.solana.martinez@gmail.com>
2020-11-06 22:21:07 +0100

    Update library

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.7.1")
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.8.2")
     ],
     targets: [
         .target(
