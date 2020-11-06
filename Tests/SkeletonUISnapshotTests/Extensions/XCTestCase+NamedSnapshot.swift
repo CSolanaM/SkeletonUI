4971111d67f1c0efb9d69607009cc2c4a61fcca6
Carlos Solana Martínez <carlos.solana.martinez@gmail.com>
2020-11-07 00:24:53 +0100

    Reference images

--- Tests/SkeletonUISnapshotTests/Extensions/XCTestCase+NamedSnapshot.swift
+++ Tests/SkeletonUISnapshotTests/Extensions/XCTestCase+NamedSnapshot.swift
@@ -28,17 +28,19 @@ extension XCTestCase {
         #endif
     }
 
-    func assertNamedSnapshot<Value, Format>(matching value: @autoclosure () throws -> Value, as snapshotting: SnapshotTesting.Snapshotting<ViewController, Format>, record recording: Bool = false, timeout: TimeInterval = 5, file: StaticString = #file, testName: String = #function, line: UInt = #line) where Value: View {
-        try? ColorScheme.allCases.forEach { scheme in
-            let view = try value().colorScheme(scheme)
-            let hostingController = HostingController(rootView: view)
-            let name = "\(device)-\(scale)-\(scheme)"
-            #if os(macOS)
-                hostingController.view.layer?.backgroundColor = .clear
-            #else
-                hostingController.view.backgroundColor = .clear
-            #endif
-            assertSnapshot(matching: hostingController, as: snapshotting, named: name, record: recording, timeout: timeout, file: file, testName: testName, line: line)
+    func assertNamedSnapshot<Value, Format>(matching value: @escaping @autoclosure () throws -> Value, as snapshotting: SnapshotTesting.Snapshotting<ViewController, Format>, record recording: Bool = false, timeout: TimeInterval = 5, file: StaticString = #file, testName: String = #function, line: UInt = #line) where Value: View {
+        DispatchQueue.main.async {
+            try? ColorScheme.allCases.forEach { scheme in
+                let view = try value().colorScheme(scheme)
+                let hostingController = HostingController(rootView: view)
+                let name = "\(self.device)-\(self.scale)-\(scheme)"
+                #if os(macOS)
+                    hostingController.view.layer?.backgroundColor = .clear
+                #else
+                    hostingController.view.backgroundColor = .clear
+                #endif
+                assertSnapshot(matching: hostingController, as: snapshotting, named: name, record: recording, timeout: timeout, file: file, testName: testName, line: line)
+            }
         }
     }
 }
