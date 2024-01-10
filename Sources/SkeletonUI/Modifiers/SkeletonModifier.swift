af650a816ae867239ff9e9b77b933b185eec1a76
Tim <sispo777@gmail.com>
2024-01-10 12:12:10 +0100

    Removed @State phase + Minor adjustments in Skeleton Modifier

--- Sources/SkeletonUI/Modifiers/SkeletonModifier.swift
+++ Sources/SkeletonUI/Modifiers/SkeletonModifier.swift
@@ -3,7 +3,6 @@ import Combine
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct SkeletonModifier: ViewModifier {
-    @State private var phase: CGFloat = 0
     let shape: ShapeType
     let animation: AnimationType
     let appearance: AppearanceType
@@ -11,11 +10,9 @@ public struct SkeletonModifier: ViewModifier {
 
     public func body(content: Content) -> some View {
         content
-            .modifier(SkeletonAnimatableModifier(phase, appearance))
+            .modifier(SkeletonAnimatableModifier(animate ? 1 : 0, appearance))
             .clipShape(SkeletonShape(shape))
-            .animation(animation.type, value: phase)
-            .onAppear {
-                phase = 1
-            }
+            .animation(animation.type, value: animate)
+            .onAppear { animate.toggle() }
     }
 }
